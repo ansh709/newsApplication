fdad777f257b6876f9852d536256148461052317
venue <82924899+ansh709@users.noreply.github.com>
2021-05-13 12:11:55 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,10 +17,10 @@ self.addEventListener("install", installEvent  => {
 	);
 });
 
-self.addEventListener("fetch", fetchEvent =>{
-	fetchEvent.respondWith(
-	  caches.match(fetchEvent.request).then(res => {
-		  return res || fetch(fetchEvent.request);
-	  })
-	);
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
 });
